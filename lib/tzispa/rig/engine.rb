fd95c0939119008a0bd8d91b42f6ed6448318933
Juan Antonio Piñero <japinero@area-integral.com>
2016-12-20 15:06:46 +0100

    new rig engine interface now shared by all apps

--- lib/tzispa/rig/engine.rb
+++ lib/tzispa/rig/engine.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'singleton'
 require 'lru_redux'
 require 'tzispa/rig/template'
 
@@ -6,54 +7,59 @@ module Tzispa
   module Rig
 
     class Engine
+      include Singleton
 
-      attr_reader :app
+      @@cache_size = 128
+      @@singleton__mutex__ = Mutex.new
 
-      def initialize(app, cache_enabled, cache_size)
-        @app = app
-        @cache = LruRedux::ThreadSafeCache.new(cache_size) if cache_enabled
+      def initialize
+        @cache = LruRedux::ThreadSafeCache.new(@@cache_size)
       end
 
-      def layout(name:, format:nil, params:nil)
-        rig_template name, :layout, format, params, nil
-      end
+      class << self
 
-      def block(name:, format:nil, params:nil, parent:nil)
-        rig_template name, :block, format, params, parent
-      end
-
-      def static(name:, format:nil, params:nil, parent:nil)
-        rig_template name, :static, format, params, parent
-      end
-
-      def rig_template(name, type, tpl_format, params, parent)
-        if @cache
-          cache_template(name, type, tpl_format, params, parent)
-        else
-          Template.new(name: name, type: type, format: tpl_format, domain: @app.domain, params: params, parent: parent, engine: self).load!.parse!
+        def layout(name:, domain:, params:nil)
+          rig_template name, domain, :layout, params
         end
+
+        def block(name:, domain:, params:nil)
+          rig_template name, domain, :block, params
+        end
+
+        def static(name:, domain:, params:nil)
+          rig_template name, domain, :static, params
+        end
+
+        def rig_template(name, domain, type, params)
+          instance.send(:cache_template, name, domain, type, params)
+        end
+
+        def cache_sizee=(sz)
+          @@cache_size = sz
+        end
+
       end
 
       private
 
-      def cache_template(name, type, tpl_format, params, parent)
-        key = "#{type}__#{name}".to_sym
-        (get_template(key, name, type, tpl_format, parent) || set_template(key, name, type, tpl_format, parent)).dup.tap { |ctpl|
+      def cache_template(name, domain, type, params)
+        key = "#{domain}__#{type}__#{name}".to_sym
+        (get_template(key, name, domain, type) || set_template(key, name, domain, type)).dup.tap { |ctpl|
            ctpl.params = params if params
          }
       end
 
-      def get_template(key, name, type, tpl_format, parent)
+      def get_template(key, name, domain, type)
         if @cache.key?(key) && (@cache[key].modified? || !@cache[key].valid?)
-          set_template(key, name, type, tpl_format, parent)
+          set_template(key, name, domain, type)
         else
           @cache[key]
         end
       end
 
-
-      def set_template(key, name, type, tpl_format, parent)
-        @cache[key] = Template.new(name: name, type: type, format: tpl_format, domain: @app.domain, parent: parent, engine: self).load!.parse!
+      def set_template(key, name, domain, type)
+        #@@singleton__mutex__.synchronize {
+        @cache[key] = Template.new(name: name, type: type, domain: domain).load!.parse!        
       end
 
     end
