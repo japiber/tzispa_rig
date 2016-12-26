58afbc2e49c12659810773eaf606854bbe75a92c
Juan Antonio Piñero <japinero@area-integral.com>
2016-12-26 11:43:32 +0100

    more testing

--- test/parsernext_test.rb
+++ test/parsernext_test.rb
@@ -1,8 +1,10 @@
 require 'fileutils'
 require 'test_helper'
+require 'tzispa/helpers/security'
 
 class ParsernextTest < Minitest::Test
   include TemplateTestHelper
+  include Tzispa::Helpers::Security
 
   def setup
     all_templates.each { |tpl|
@@ -25,6 +27,7 @@ class ParsernextTest < Minitest::Test
     assert_equal parser.the_parsed.count, 3
     assert_equal parser.attribute_tags.count, 2
     assert_equal parser.attribute_tags, [:meta1, :meta2]
+    assert_equal parser.attribute_tags, [:meta1, :meta2]
     assert_instance_of Tzispa::Rig::ParsedMeta, parser.the_parsed[0]
     assert_instance_of Tzispa::Rig::ParsedMeta, parser.the_parsed[1]
     assert_instance_of Tzispa::Rig::ParsedMeta, parser.the_parsed[2]
@@ -36,19 +39,19 @@ class ParsernextTest < Minitest::Test
   def test_meta_render
     parser = Tzispa::Rig::ParserNext.new TPL_META, domain: domain, content_type: :txt, bindable: true
     parser.parse!
-    assert_equal parser.attribute_tags.count, 2
-    assert_equal parser.attribute_tags, [:meta1, :meta2]
-    binder = binder_fake.new parser, {}, [123, 'john doe']
+    binder = binder_fake.new parser, context, [123, 'john doe']
     assert_equal parser.render(binder), 'meta testing 123\n\n 123:john doe\n'
-    binder = binder_fake.new parser, {}, [nil, 'john doe']
+    binder = binder_fake.new parser, context, [nil, 'john doe']
     assert_equal parser.render(binder), 'meta testing \n\n :john doe\n'
-    binder = binder_fake.new parser, {}, []
+    binder = binder_fake.new parser, context, []
     assert_equal parser.render(binder), 'meta testing \n\n :\n'
   end
 
   def test_var_parser
     parser = Tzispa::Rig::ParserNext.new(TPL_VAR, domain: domain, content_type: :htm, bindable: true).parse!
     assert_equal parser.the_parsed.count, 5
+    assert_equal parser.attribute_tags.count, 4
+    assert_equal parser.attribute_tags, [:meta1, :meta2, :uno, :dos]
     # metas are parsed before vars
     assert_instance_of Tzispa::Rig::ParsedVar, parser.the_parsed[2]
     assert_instance_of Tzispa::Rig::ParsedVar, parser.the_parsed[3]
@@ -60,14 +63,11 @@ class ParsernextTest < Minitest::Test
 
   def test_var_render
     parser = Tzispa::Rig::ParserNext.new(TPL_VAR, domain: domain, content_type: :htm, bindable: true).parse!
-    assert_equal parser.the_parsed.count, 5
-    assert_equal parser.attribute_tags.count, 4
-    assert_equal parser.attribute_tags, [:meta1, :meta2, :uno, :dos]
-    binder = binder_fake.new parser, {}, [2016, 'john doe', 'happy', 'year']
+    binder = binder_fake.new parser, context, [2016, 'john doe', 'happy', 'year']
     assert_equal parser.render(binder), 'var testing happy\n\n 2016 year  happy:john doe\n'
-    binder = binder_fake.new parser, {}, [2015, 'john doe', 'happy']
+    binder = binder_fake.new parser, context, [2015, 'john doe', 'happy']
     assert_equal parser.render(binder), 'var testing happy\n\n 2015   happy:john doe\n'
-    binder = binder_fake.new parser, {}, []
+    binder = binder_fake.new parser, context, []
     assert_equal parser.render(binder), 'var testing \n\n    :\n'
   end
 
@@ -89,17 +89,17 @@ class ParsernextTest < Minitest::Test
 
   def test_loop_render
     parser = Tzispa::Rig::ParserNext.new(TPL_LOOP, domain: domain, content_type: :htm, bindable: true).parse!
-    binder = binder_fake.new parser, {}, [Struct.new(:data).new([
+    binder = binder_fake.new parser, context, [Struct.new(:data).new([
       binder_fake.new(parser.the_parsed[0].body_parser, {}, [1, 2]),
       binder_fake.new(parser.the_parsed[0].body_parser, {}, [3, 4]),
       binder_fake.new(parser.the_parsed[0].body_parser, {}, [5, 6])
       ]), 'watching']
     assert_equal parser.render(binder), ' loop testing 1 2\n 1  loop testing 3 4\n 3  loop testing 5 6\n 5  watching'
-    binder = binder_fake.new parser, {}, [Struct.new(:data).new([]), 'watching']
+    binder = binder_fake.new parser, context, [Struct.new(:data).new([]), 'watching']
     assert_equal parser.render(binder), ' watching'
-    binder = binder_fake.new parser, {}, [Struct.new(:data).new, 'watching']
+    binder = binder_fake.new parser, context, [Struct.new(:data).new, 'watching']
     assert_raises(NoMethodError) { parser.render(binder) }
-    binder = binder_fake.new parser, {}, [[], 'watching']
+    binder = binder_fake.new parser, context, [[], 'watching']
     assert_raises(NoMethodError) { parser.render(binder) }
   end
 
@@ -123,15 +123,15 @@ class ParsernextTest < Minitest::Test
 
   def test_ife_render
     parser = Tzispa::Rig::ParserNext.new(TPL_IFE, domain: domain, content_type: :htm, bindable: true).parse!
-    binder = binder_fake.new parser, {}, [1==1, 'john doe', 'happy', 'year']
+    binder = binder_fake.new parser, context, [1==1, 'john doe', 'happy', 'year']
     assert_equal parser.render(binder), ' ife testing john doe happy\n   john doe happy year  '
-    binder = binder_fake.new parser, {}, [1==0, 'john doe', 'happy', 'year']
+    binder = binder_fake.new parser, context, [1==0, 'john doe', 'happy', 'year']
     assert_equal parser.render(binder), '  happy  '
   end
 
 
   def test_url_parser
-    parser = Tzispa::Rig::ParserNext.new(TPL_URL, domain: domain, content_type: :htm, bindable: true).parse!
+    parser = Tzispa::Rig::ParserNext.new("#{TPL_URL1} #{TPL_URL2} #{TPL_URL3}", domain: domain, content_type: :htm, bindable: true).parse!
     assert_equal parser.the_parsed.count, 4
     assert_instance_of Tzispa::Rig::ParsedUrl, parser.the_parsed[1]
     assert_instance_of Tzispa::Rig::ParsedUrl, parser.the_parsed[2]
@@ -141,14 +141,29 @@ class ParsernextTest < Minitest::Test
     assert_equal parser.the_parsed[3].layout, 'article_edit'
     assert_equal parser.the_parsed[1].params, 'id=111,title=this_is_an_url_title'
     assert_equal parser.the_parsed[2].params, "id=#{parser.the_parsed[0].anchor},title=this_is_an_url_title"
-    assert_equal parser.the_parsed[3].params, 'id=122,format=json'
+    assert_equal parser.the_parsed[3].params, 'id=1220,format=json'
     assert_nil parser.the_parsed[1].app_name
     assert_nil parser.the_parsed[2].app_name
     assert_equal parser.the_parsed[3].app_name, :adminapp
   end
 
+  def test_url_render
+    parser = Tzispa::Rig::ParserNext.new(TPL_URL1, domain: domain, content_type: :htm, bindable: true).parse!
+    binder = binder_fake.new parser, context, []
+    assert_equal parser.render(binder), 'http://mytestdomainurl.com/article/this_is_an_url_title/111'
+
+    parser = Tzispa::Rig::ParserNext.new(TPL_URL2, domain: domain, content_type: :htm, bindable: true).parse!
+    binder = binder_fake.new parser, context, [2091]
+    assert_equal parser.render(binder), '/article_list/this_is_an_url_title/2091'
+
+    parser = Tzispa::Rig::ParserNext.new(TPL_URL3, domain: domain, content_type: :htm, bindable: true).parse!
+    binder = binder_fake.new parser, context, []
+    assert_equal parser.render(binder), '/adminapp/article_edit/1220.json'
+  end
+
+
   def test_api_parser
-    parser = Tzispa::Rig::ParserNext.new(TPL_API, domain: domain, content_type: :htm, bindable: true).parse!
+    parser = Tzispa::Rig::ParserNext.new("#{TPL_API1} #{TPL_API2} #{TPL_API3}", domain: domain, content_type: :htm, bindable: true).parse!
     assert_equal parser.the_parsed.count, 4
     assert_instance_of Tzispa::Rig::ParsedApi, parser.the_parsed[1]
     assert_instance_of Tzispa::Rig::ParsedApi, parser.the_parsed[2]
@@ -166,6 +181,23 @@ class ParsernextTest < Minitest::Test
     assert_equal parser.the_parsed[3].predicate, '2016_10,2016_12'
     assert_nil parser.the_parsed[3].app_name
   end
+
+  def test_api_render
+    parser = Tzispa::Rig::ParserNext.new(TPL_API1, domain: domain, content_type: :htm, bindable: true).parse!
+    binder = binder_fake.new parser, context, []
+    assert_equal parser.render(binder), 'http://mytestdomainurl.com/article/add'
+
+    parser = Tzispa::Rig::ParserNext.new(TPL_API2, domain: domain, content_type: :htm, bindable: true).parse!
+    binder = binder_fake.new parser, context, [11999]
+    assert_equal parser.render(binder), 'http://admin.mytestdomainurl.com/adminapp/article/edit/11999'
+
+    sign = context.sign_array ['order', 'detail_sum', '2016_10,2016_12']
+    parser = Tzispa::Rig::ParserNext.new(TPL_API3, domain: domain, content_type: :htm, bindable: true).parse!
+    binder = binder_fake.new parser, context, []
+    assert_equal parser.render(binder), "http://mytestdomainurl.com/#{sign}__order/detail_sum/2016_10,2016_12"
+
+  end
+
 
   def test_blk_parser
     parser = Tzispa::Rig::ParserNext.new(TPL_BLK, domain: domain, content_type: :htm, bindable: true).parse!
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,23 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'tzispa_rig'
 require 'minitest/autorun'
+require 'tzispa/helpers/security'
 
 
 module TemplateTestHelper
   extend Minitest::Spec::DSL
+  include Tzispa::Helpers::Security
 
   TPL_META = 'meta testing {%meta1%}\n\n {%meta1%}:{%meta2%}\n'
   TPL_VAR  = 'var testing <var:uno/>\n\n {%meta1%} <var:dos/>  <var:uno/>:{%meta2%}\n'
   TPL_LOOP = '<loop:literator> loop testing <var:uno/> <var:dos/>\n <var:uno/> </loop:literator> <var:tres/>'
   TPL_IFE  = '<ife:condition> ife testing <var:uno/> <var:dos/>\n </ife:condition> <ife:condition> <var:uno/> {%dos%} {%tres%} <else:condition/> <var:dos/> </ife:condition> '
-  TPL_URL  = '<url:article[id=111,title=this_is_an_url_title]/> <purl:article_list[id={%idp%},title=this_is_an_url_title]/> <purl@adminapp:article_edit[id=122,format=json]/>'
-  TPL_API  = '<api:article:add/> <api@adminapp:article:edit:{%idarticle%}/> <sapi:order:detail_sum:2016_10,2016_12/>'
+  TPL_URL1  = '<url:article[id=111,title=this_is_an_url_title]/>'
+  TPL_URL2  = '<purl:article_list[id={%idp%},title=this_is_an_url_title]/>'
+  TPL_URL3  = '<purl@adminapp:article_edit[id=1220,format=json]/>'
+  TPL_API1  = '<api:article:add/>'
+  TPL_API2  = '<api@adminapp:article:edit:{%idarticle%}/>'
+  TPL_API3  = '<sapi:order:detail_sum:2016_10,2016_12/>'
   TPL_BLK  = '<blk:detail/> <blk:product.detail[tab={%selected_tab%}]/> <iblk:test:block_one[select=50]:product.block_two/> <iblk:test:product.block_one:block_two[select=10]/>'
   TPL_STA  = '<static:whatsnew/> <static:product.whatsnew[section=retail]/> <static:product.whatsnew[section={%selected%}]/>'
 
@@ -60,6 +66,109 @@ module TemplateTestHelper
         data_struct.new(*values)
       end
     }
+  }
+
+  let(:config_fake) {
+    Struct.new(:canonical_url)
+  }
+
+  let(:app_fake) {
+    Struct.new(:config)
+  }
+
+  let(:context_fake) {
+    Struct.new(:app, :applications, :salt) {
+
+      def layout_path(layout, params={})
+        String.new.tap { |path|
+          path << "/#{layout}"
+          path << "/#{params[:title]}" if params[:title]
+          user_params = params.reject { |k,v| k == :title || k == :format}
+          path << "/#{user_params.values.join('__')}" if user_params.size > 0
+          path << ".#{params[:format]}" if params[:format]
+        }
+      end
+
+      def app_layout_path(app_name, layout, params={})
+        String.new.tap { |path|
+          path << "/#{app_name}"
+          path << "/#{layout}"
+          path << "/#{params[:title]}" if params[:title]
+          user_params = params.reject { |k,v| k == :title || k == :format}
+          path << "/#{user_params.values.join('__')}" if user_params.size > 0
+          path << ".#{params[:format]}" if params[:format]
+        }
+      end
+
+      def layout_canonical_url(layout, params={})
+        String.new.tap { |path|
+          path << "#{app.config.canonical_url}"
+          path << "/#{layout}"
+          path << "/#{params[:title]}" if params[:title]
+          user_params = params.reject { |k,v| k == :title || k == :format}
+          path << "/#{user_params.values.join('__')}" if user_params.size > 0
+          path << ".#{params[:format]}" if params[:format]
+        }
+      end
+
+      def app_layout_canonical_url(app_name, layout, params={})
+        String.new.tap { |path|
+          path << "#{applications[app_name].config.canonical_url}"
+          path << "/#{app_name}"
+          path << "/#{layout}"
+          path << "/#{params[:title]}" if params[:title]
+          user_params = params.reject { |k,v| k == :title || k == :format}
+          path << "/#{user_params.values.join('__')}" if user_params.size > 0
+          path << ".#{params[:format]}" if params[:format]
+        }
+      end
+
+      def api(handler, verb, predicate, sufix, app_name = nil)
+        String.new.tap { |path|
+          path << (app_name ?
+                  "#{applications[app_name].config.canonical_url}" :
+                  "#{app.config.canonical_url}")
+          path << "/#{app_name}" if app_name
+          path << "/#{handler}/#{verb}"
+          path << "/#{predicate}" if predicate
+          path << ".#{sufix}" if sufix
+        }
+      end
+
+      def sapi(handler, verb, predicate, sufix, app_name = nil)
+        sign = sign_array [handler, verb, predicate]
+        String.new.tap { |path|
+          path << (app_name ? "#{applications[app_name].config.canonical_url}" : "#{app.config.canonical_url}")
+          path << "/#{app_name}" if app_name
+          path << "/#{sign}__#{handler}/#{verb}"
+          path << "/#{predicate}" if predicate
+          path << ".#{sufix}" if sufix
+        }
+      end
+
+      def sign_array(astr)
+        sign, i = String.new, 0
+        astr.each { |s|
+          i = i + 1
+          sign << "#{"_"*i}#{s}"
+        }
+        sign << "**#{salt}"
+        Digest::SHA1.hexdigest sign
+      end
+
+    }
+  }
+
+  let(:context) {
+    cfg_main = config_fake.new 'http://mytestdomainurl.com'
+    cfg_admin = config_fake.new 'http://admin.mytestdomainurl.com'
+    app_main = app_fake.new cfg_main
+    app_admin = app_fake.new cfg_admin
+    applications = {
+      main: app_main,
+      adminapp: app_admin
+    }
+    context_fake.new app_main, applications, 'qwertyuiop0987654321'
   }
 
 end
