6f47916e5b41fb1ae2bb9d1e4783e4b0f85aae8b
Juan Antonio Piñero <japinero@area-integral.com>
2017-02-10 16:06:18 +0100

    requires add and organization

--- lib/tzispa/rig.rb
+++ lib/tzispa/rig.rb
@@ -2,9 +2,10 @@ module Tzispa
   module Rig
 
     require 'tzispa/rig/version'
-    require 'tzispa/rig/template'
     require 'tzispa/rig/parameters'
+    require 'tzispa/rig/token'
     require 'tzispa/rig/parsernext'
+    require 'tzispa/rig/template'        
     require 'tzispa/rig/binder'
     require 'tzispa/rig/engine'
 
