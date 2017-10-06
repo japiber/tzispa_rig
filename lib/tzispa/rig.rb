3ed603f6c0421ae45f407bd9bbcaa8fc148e6cc5
Juan Antonio Piñero <japinero@area-integral.com>
2017-10-06 21:30:51 +0200

    classes autoload

--- lib/tzispa/rig.rb
+++ lib/tzispa/rig.rb
@@ -3,13 +3,14 @@
 module Tzispa
   module Rig
 
-    require 'tzispa/rig/version'
-    require 'tzispa/rig/parameters'
-    require 'tzispa/rig/token'
-    require 'tzispa/rig/parsernext'
-    require 'tzispa/rig/template'
-    require 'tzispa/rig/binder'
-    require 'tzispa/rig/factory'
+    autoload :Parameters,         'tzispa/rig/parameters'
+    autoload :Token,              'tzispa/rig/token'
+    autoload :Parsernext,         'tzispa/rig/parser_next'
+    autoload :Template,           'tzispa/rig/template'
+    autoload :Binder,             'tzispa/rig/binder'
+    autoload :Factory,            'tzispa/rig/factory'
+    autoload :Handler,            'tzispa/rig/handler'
+    autoload :AnnotatedHandler,   'tzispa/rig/annotated_handler'
 
   end
 end
